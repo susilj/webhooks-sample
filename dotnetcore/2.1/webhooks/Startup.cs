03a68f543c3ed1f8988c88897ca8b9ce19d499c7
Susil Kumar J <susil_j@hotmail.com>
2018-08-08 23:06:44 +0530

    Removed HTTPS Redirection

--- dotnetcore/2.1/webhooks/Startup.cs
+++ dotnetcore/2.1/webhooks/Startup.cs
@@ -50,7 +50,7 @@ namespace webhooks
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
